58939c926d6332ea2e46d740a1457bae0f365ddd
TechBuzzz <buzin.victor@gmail.com>
2014-11-08 14:46:47 +0300

    add extensions

---
+++ src/2K.Common/Files/ImageHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace _2K.Common.Files
+{
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// Creates a thumbnail to a given image.
+        /// </summary>
+        /// <param name="file">The image.</param>
+        /// <param name="width">The maximum width of the thumbnail.</param>
+        /// <param name="height">The maximum height of the thumbnail.</param>
+        /// <param name="relative">Determines whether the image is resized relatively.</param>
+        /// <returns>The resized image.</returns>
+        public static byte[] MakeThumbnail(byte[] file, int width, int height, bool relative)
+        {
+            Image image = null;
+
+            using (var input = new MemoryStream(file))
+            {
+                image = Image.FromStream(new MemoryStream(file));
+            }
+
+            if (relative)
+            {
+                if (image.Width > image.Height)
+                {
+                    height = image.Height * width / image.Width;
+                    if (width > image.Width)
+                    {
+                        return file;
+                    }
+                }
+                else
+                {
+                    width = image.Width * height / image.Height;
+                    if (height > image.Height)
+                    {
+                        return file;
+                    }
+                }
+            }
+
+            var scaledImage = image.GetThumbnailImage(width, height, null, new System.IntPtr());
+            using (var result = new MemoryStream())
+            {
+                scaledImage.Save(result, ImageFormat.Jpeg);
+                return result.ToArray();
+            }
+        }
+    }
+}
